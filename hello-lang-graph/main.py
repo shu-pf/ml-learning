0f7d6eb4fa79bc87cf3125381d19b9f9613f3c9a
Furuse Shugo <shugo.furuse@accenture.com>
2025-12-23 16:48:27 +0900

    a

--- hello-lang-graph/main.py
+++ hello-lang-graph/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from hello-lang-graph!")
+from langgraph.graph import StateGraph, MessagesState, START, END
+from langchain_core.messages import HumanMessage
 
+def mock_llm(state: MessagesState):
+    return {"messages": [{"role": "ai", "content": "hello world"}]}
 
-if __name__ == "__main__":
-    main()
+graph = StateGraph(MessagesState)
+graph.add_node(mock_llm)
+graph.add_edge(START, "mock_llm")
+graph.add_edge("mock_llm", END)
+graph = graph.compile()
+
+result = graph.invoke({"messages": [HumanMessage(content="hi!")]})
+print(result)
